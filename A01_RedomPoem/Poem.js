d87e821efde76ced72e3d729e71d3aa1f1e276d4
K11wiky <115713151+K11wiky@users.noreply.github.com>
2023-04-03 13:19:43 +0200

    commit

--- A01_RedomPoem/Poem.js
+++ A01_RedomPoem/Poem.js
@@ -1,56 +1,34 @@
 var Gedicht;
 (function (Gedicht) {
-    // Arrays für Subjekte, Verben und Objekte
-    let subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape"];
-    let verbs = ["braut", "liebt", "studiert", "hasst", "zaubert"];
-    let objects = ["Zaubertränke", "Lupin", "Hermine", "den Stein der Weisen", "Quidditch"];
-    // Funktion, die ein Gedicht generiert
-    function generatePoem() {
-        let verse = ""; // leere Vers-Variable
-        let randSubjectIndex = Math.floor(Math.random() * subjects.length); // zufällige Indexauswahl für Subjekt
-        let randVerbIndex = Math.floor(Math.random() * verbs.length); // zufällige Indexauswahl für Verb
-        let randObjectIndex = Math.floor(Math.random() * objects.length); // zufällige Indexauswahl für Objekt
-        // Füge das zufällig ausgewählte Subjekt, Verb und Objekt zum Vers hinzu
-        verse += subjects.splice(randSubjectIndex, 1)[0]; // Schneide das ausgewählte Subjekt aus dem Array und füge es zum Vers hinzu
-        verse += " ";
-        verse += verbs.splice(randVerbIndex, 1)[0]; // Schneide das ausgewählte Verb aus dem Array und füge es zum Vers hinzu
-        verse += " ";
-        verse += objects.splice(randObjectIndex, 1)[0]; // Schneide das ausgewählte Objekt aus dem Array und füge es zum Vers hinzu
-        // Wiederhole das Gleiche für die restlichen Zeilen
-        randSubjectIndex = Math.floor(Math.random() * subjects.length);
-        randVerbIndex = Math.floor(Math.random() * verbs.length);
-        randObjectIndex = Math.floor(Math.random() * objects.length);
-        verse += "\n";
-        verse += subjects.splice(randSubjectIndex, 1)[0];
-        verse += " ";
-        verse += verbs.splice(randVerbIndex, 1)[0];
-        verse += " ";
-        verse += objects.splice(randObjectIndex, 1)[0];
-        randSubjectIndex = Math.floor(Math.random() * subjects.length);
-        randVerbIndex = Math.floor(Math.random() * verbs.length);
-        randObjectIndex = Math.floor(Math.random() * objects.length);
-        verse += "\n";
-        verse += subjects.splice(randSubjectIndex, 1)[0];
-        verse += " ";
-        verse += verbs.splice(randVerbIndex, 1)[0];
-        verse += " ";
-        verse += objects.splice(randObjectIndex, 1)[0];
-        randSubjectIndex = Math.floor(Math.random() * subjects.length);
-        randVerbIndex = Math.floor(Math.random() * verbs.length);
-        randObjectIndex = Math.floor(Math.random() * objects.length);
-        verse += "\n";
-        verse += subjects.splice(randSubjectIndex, 1)[0];
-        verse += " ";
-        verse += verbs.splice(randVerbIndex, 1)[0];
-        verse += " ";
-        verse += objects.splice(randObjectIndex, 1)[0];
-        return verse; // Rückgabe des Gedichts
+    //Array mit möglichen Subjekten
+    const subjects = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    const predicates = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    const objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    // Funktion, die einen Vers generiert und zurück gibt
+    function getVerse(_subjects, _predicates, _objects) {
+        // Initialisieren einer leeren Vers-Variable
+        let verse = "";
+        // Zufälliges Subjekt auswählen und aus dem Array entfernen
+        let subjectIndex = Math.floor(Math.random() * _subjects.length);
+        verse += _subjects.splice(subjectIndex, 1)[0] + " ";
+        // Zufälliges Prädikat auswählen und aus dem Array entfernen
+        let predicateIndex = Math.floor(Math.random() * _predicates.length);
+        verse += _predicates.splice(predicateIndex, 1)[0] + " ";
+        // Zufälliges Objekt auswählen und aus dem Array entfernen
+        let objectIndex = Math.floor(Math.random() * _objects.length);
+        verse += _objects.splice(objectIndex, 1)[0] + ".";
+        // Den generierten Vers zurück geben
+        return verse;
     }
-    Gedicht.generatePoem = generatePoem;
-    // Debugging-Kommentare
-    console.log("Start");
-    console.log(generatePoem());
-    console.log(generatePoem());
-    console.log(generatePoem());
+    // Initialisieren einer leeren Gedicht-Variable
+    let poem = "";
+    // Schleife, die so oft durchläuft, wie es Elemente im Subjekte-Array gibt
+    for (let i = subjects.length; i >= 1; i--) {
+        // Einen Vers generieren und an die Gedicht-Variable anhängen
+        let verse = getVerse(subjects, predicates, objects);
+        poem += verse + "\n";
+    }
+    // Gedicht auf der Konsole ausgeben
+    console.log(poem);
 })(Gedicht || (Gedicht = {}));
 //# sourceMappingURL=Poem.js.map
--- A01_RedomPoem/Poem.ts
+++ A01_RedomPoem/Poem.ts
@@ -1,60 +1,41 @@
 namespace Gedicht {
-    // Arrays für Subjekte, Verben und Objekte
-    let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape"];
-    let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert"];
-    let objects: string[] = ["Zaubertränke", "Lupin", "Hermine", "den Stein der Weisen", "Quidditch"];
-  
-    // Funktion, die ein Gedicht generiert
-    export function generatePoem(): string {
-      let verse: string = ""; // leere Vers-Variable
-      let randSubjectIndex: number = Math.floor(Math.random() * subjects.length); // zufällige Indexauswahl für Subjekt
-      let randVerbIndex: number = Math.floor(Math.random() * verbs.length); // zufällige Indexauswahl für Verb
-      let randObjectIndex: number = Math.floor(Math.random() * objects.length); // zufällige Indexauswahl für Objekt
-  
-      // Füge das zufällig ausgewählte Subjekt, Verb und Objekt zum Vers hinzu
-      verse += subjects.splice(randSubjectIndex, 1)[0]; // Schneide das ausgewählte Subjekt aus dem Array und füge es zum Vers hinzu
-      verse += " ";
-      verse += verbs.splice(randVerbIndex, 1)[0]; // Schneide das ausgewählte Verb aus dem Array und füge es zum Vers hinzu
-      verse += " ";
-      verse += objects.splice(randObjectIndex, 1)[0]; // Schneide das ausgewählte Objekt aus dem Array und füge es zum Vers hinzu
-  
-      // Wiederhole das Gleiche für die restlichen Zeilen
-      randSubjectIndex = Math.floor(Math.random() * subjects.length);
-      randVerbIndex = Math.floor(Math.random() * verbs.length);
-      randObjectIndex = Math.floor(Math.random() * objects.length);
-      verse += "\n";
-      verse += subjects.splice(randSubjectIndex, 1)[0];
-      verse += " ";
-      verse += verbs.splice(randVerbIndex, 1)[0];
-      verse += " ";
-      verse += objects.splice(randObjectIndex, 1)[0];
-  
-      randSubjectIndex = Math.floor(Math.random() * subjects.length);
-      randVerbIndex = Math.floor(Math.random() * verbs.length);
-      randObjectIndex = Math.floor(Math.random() * objects.length);
-      verse += "\n";
-      verse += subjects.splice(randSubjectIndex, 1)[0];
-      verse += " ";
-      verse += verbs.splice(randVerbIndex, 1)[0];
-      verse += " ";
-      verse += objects.splice(randObjectIndex, 1)[0];
-  
-      randSubjectIndex = Math.floor(Math.random() * subjects.length);
-      randVerbIndex = Math.floor(Math.random() * verbs.length);
-      randObjectIndex = Math.floor(Math.random() * objects.length);
-      verse += "\n";
-      verse += subjects.splice(randSubjectIndex, 1)[0];
-      verse += " ";
-      verse += verbs.splice(randVerbIndex, 1)[0];
-      verse += " ";
-      verse += objects.splice(randObjectIndex, 1)[0];
-  
-      return verse; // Rückgabe des Gedichts
-    }
-  
-    // Debugging-Kommentare
-    console.log("Start");
-    console.log(generatePoem());
-    console.log(generatePoem());
-    console.log(generatePoem());
+  //Array mit möglichen Subjekten
+  const subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+const predicates: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+const objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+// Funktion, die einen Vers generiert und zurück gibt
+function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+  // Initialisieren einer leeren Vers-Variable
+  let verse = "";
+
+  // Zufälliges Subjekt auswählen und aus dem Array entfernen
+  let subjectIndex = Math.floor(Math.random() * _subjects.length);
+  verse += _subjects.splice(subjectIndex, 1)[0] + " ";
+
+  // Zufälliges Prädikat auswählen und aus dem Array entfernen
+  let predicateIndex = Math.floor(Math.random() * _predicates.length);
+  verse += _predicates.splice(predicateIndex, 1)[0] + " ";
+
+  // Zufälliges Objekt auswählen und aus dem Array entfernen
+  let objectIndex = Math.floor(Math.random() * _objects.length);
+  verse += _objects.splice(objectIndex, 1)[0] + ".";
+
+  // Den generierten Vers zurück geben
+  return verse;
+}
+
+// Initialisieren einer leeren Gedicht-Variable
+let poem = "";
+
+// Schleife, die so oft durchläuft, wie es Elemente im Subjekte-Array gibt
+for (let i = subjects.length; i >= 1; i--) {
+  // Einen Vers generieren und an die Gedicht-Variable anhängen
+  let verse = getVerse(subjects, predicates, objects);
+  poem += verse + "\n";
+}
+
+// Gedicht auf der Konsole ausgeben
+console.log(poem);
+ 
 }
